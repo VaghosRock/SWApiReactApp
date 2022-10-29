243b976c95a0760a47da5dba6243e5fca395aa88
VaghosRock <luks91@live.com.ar>
2022-10-29 13:44:23 -0300

    Primera parte, fetch funcional y colocacion de cartas

--- miapp/src/App.js
+++ miapp/src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Nombres from "./Components/Nombres";
+
 
 function App() {
+  const urlInicial = "https://swapi.dev/api/people";
+  const [url, setUrl] = useState(urlInicial);
+  const [datos, setDatos] = useState([]);
+  const [personajes, setPersonajes] = useState([]);
+  function fetchApi(url) {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setDatos(data);
+        setPersonajes(data.results);
+        console.log(data);
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }
+  useEffect(() => {
+      fetchApi(url);
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Lucas</h1>
-    </div>
+    <>
+      <div className="App">Star Wars Cards</div>
+
+      <Nombres personajes={personajes} />
+    </>
   );
 }
 
